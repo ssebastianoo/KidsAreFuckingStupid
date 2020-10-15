d8afbb1973d26b312eadcec62d67c1399fa88790
ssebastianoo <sebastianogirotto05@gmail.com>
2020-10-15 20:43:30 +0200

    oof

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import telepot, requests, json, time, os, dotenv
 
 dotenv.load_dotenv(dotenv_path = ".env")
 
+def bitly(url):
+    headers = {
+    'Authorization': f'Bearer {os.environ["bitly"]}',
+    'Content-Type': 'application/json',
+    }
+
+    data = '{"long_url": "' + url + '", "domain": "bit.ly"}'
+
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+    res = response.json()
+
+    return res["link"]
+
 bot = telepot.Bot(os.environ["token"])
 print("ready")
 
 while True:
-    url = "https://www.reddit.com/r/KidsAreFuckingStupid/new.json"
+    url = "https://www.reddit.com/r/KidsAreFuckingStupid/hot.json"
     res = requests.get(url, headers = {'User-agent': 'sebamemes by u/sebastianogirotto'})
     res = res.json()
     url = res["data"]["children"][0]["data"]["url_overridden_by_dest"]
@@ -14,6 +27,8 @@ while True:
     subreddit = "r/" + res["data"]["children"][0]["data"]["subreddit"]
     ups = res["data"]["children"][0]["data"]["ups"]
     title = res["data"]["children"][0]["data"]["title"]
+    link = "https://reddit.com" + res["data"]["children"][0]["data"]["permalink"]
+    link = bitly(link)
 
     if "v.redd.it" in url:
         url = res["data"]["children"][0]["data"]["media"]["reddit_video"]["fallback_url"]
@@ -33,17 +48,12 @@ while True:
         f.write(url)
         f.close()
 
-        caption = f"• *{title}*\n\n• by *{author}*\n\n• *{ups}* upvotes"
-
-        # parse_mode = "Markdown"
+        caption = f"• *{title}*\n\n• by *{author}*\n\n• *{ups}* upvotes\n\n• {link}"
 
         if url.endswith(("png", "jpg", "jpeg")):
             bot.sendPhoto(-1001410145919, url, caption = caption, parse_mode = "Markdown")
-
-        elif ".gif" in url:
-            bot.sendAnimation(-1001410145919, url, caption = caption, parse_mode = "Markdown")
         
-        elif ".mp4" in url:
+        elif ".mp4" in url or ".gif" in url:
             bot.sendVideo(-1001410145919, url, caption = caption, parse_mode = "Markdown")
 
-    time.sleep(120)
+    time.sleep(120) 
